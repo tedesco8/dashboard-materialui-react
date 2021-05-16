0c2dfea864d913b577a75ea4434994f689b10cd0
Pablo Tedesco <tedesco8@hotmail.com>
2021-05-15 21:10:54 -0300

    ED: Creando estructura basica e instalando dependencias

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# dashboard-materialui-react
+Dashboard sencillo y responsive con React JS y Material UI (Material Design)
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            Soy el Dashboard
+        </div>
+    );
+}
+
+export default Dashboard;
